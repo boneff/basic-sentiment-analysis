8def66954850b6ba37001983d61e9eb9b991c297
Iliyan <boneff.i@gmail.com>
2022-12-10 11:56:43 +0200

    Parametrize the CSV and field to be analyzed, add docker-compose and amend readme

--- README.md
+++ README.md
@@ -19,6 +19,7 @@ docker push boneff/basic-python-sentiment-analysis:latest
 ```
 
 # Usage
+Runs with default values and a CSV pre-baked into the Docker image.
 
 ```commandline
 docker run --rm -it boneff/basic-python-sentiment-analysis:latest bash
@@ -26,4 +27,19 @@ docker run --rm -it boneff/basic-python-sentiment-analysis:latest bash
 
 ```commandline
 pipenv run python main.py
+```
+
+Running with Docker Compose for and has mounted the project dir to the image, so we can debug in the container or run with custom parameters:
+
+```commandline
+docker-compose up -d
+```
+
+Accepted parameters as environment variables are `CSV_PATH` and `FIELD_NAME`.
+`CSV_PATH` - path to the CSV you want to analyze.
+`FIELD_NAME` - the name of the field from the CSV you want to run sentiment analysis on.
+
+Then exec into it
+```commandline
+docker exec -it -e CSV_PATH=/app/data/avatar_modified.csv -e FIELD_NAME=character_lines  basic-sentiment-analysis-python_app-1 bash
 ```
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import pandas as pd
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from datetime import datetime
+import logging
+import os
+
+
+def setup():
+    # datetime object containing current date and time
+    now = datetime.now()
+    datetime_string = now.strftime("%Y%m%d-%H%M%S")
+    env = os.getenv('ENVIRONMENT')
+
+    logging.basicConfig(filename='./logs/crawler-{}-{}.log'.format(datetime_string, env), encoding='utf-8', level=logging.DEBUG)
+
+
 
 # reading and wrangling data
-df_avatar = pd.read_csv('/app/data/avatar.csv', encoding='unicode_escape', engine='python')
-df_avatar_lines = df_avatar.groupby('character').count()
-df_avatar_lines = df_avatar_lines.sort_values(by=['character_words'], ascending=False)[:10]
-top_character_names = df_avatar_lines.index.values
-# filtering out non-top characters
-df_character_sentiment = df_avatar[df_avatar['character'].isin(top_character_names)]
-df_character_sentiment = df_character_sentiment[['character', 'character_words']]
+# TODO - pass path to the CSV
+# TODO - change the example to mount a volume, so we can read the file from the passed path
+# TODO - pass the fields we want to work with from the CSV
+csv_path = os.getenv('CSV_PATH', '/app/data/avatar.csv')
+field_name = os.getenv('FIELD_NAME', 'character_words')
+
+df_avatar = pd.read_csv(csv_path, encoding='unicode_escape', engine='python')
+df_character_sentiment = df_avatar[df_avatar[field_name].str.len() >= 1]
 
 # calculating sentiment score
 sid = SentimentIntensityAnalyzer()
 df_character_sentiment.reset_index(inplace=True, drop=True)
-df_character_sentiment[['neg', 'neu', 'pos', 'compound']] = df_character_sentiment['character_words'].apply(sid.polarity_scores).apply(pd.Series)
+print(df_character_sentiment)
+df_character_sentiment[['neg', 'neu', 'pos', 'compound']] = df_character_sentiment[field_name].apply(sid.polarity_scores).apply(pd.Series)
 print(df_character_sentiment)
